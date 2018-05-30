6f46a7e50c1c48a0da9eb30dde37afd4a574ca62
DanielManrique <daniel_cifuentes82142@elpoli.edu.co>
2018-05-30 12:36:06 -0500

    Final ahora si validado full

--- WebSite2/Views/Venta.aspx.cs
+++ WebSite2/Views/Venta.aspx.cs
@@ -9,9 +9,6 @@ using System.IO;
 
 public partial class Venta : System.Web.UI.Page
 {
-
-   
-
     protected void Page_Load(object sender, EventArgs e)
     {
         GestionarUsuario GestUser = new GestionarUsuario();
@@ -69,7 +66,7 @@ public partial class Venta : System.Web.UI.Page
 
         if (txtIdcompra.Text.Length > 0)
         {
-           var Lista =  LstCompras.Where(x => x.Activo == true && x.IdRegistroCompra == Convert.ToUInt32(txtIdcompra.Text)).ToList();
+            var Lista = LstCompras.Where(x => x.Activo == true && x.IdRegistroCompra == Convert.ToUInt32(txtIdcompra.Text)).ToList();
 
             if (Lista.Count > 0)
             {
@@ -88,41 +85,39 @@ public partial class Venta : System.Web.UI.Page
                 int rowcount = GridViewCompras.Rows.Count;
                 for (int i = 0; i < rowcount; i++)
                 {
-                    if(GridViewCompras.Rows[i].Cells[0].Text == txtIdcompra.Text)
+                    if (GridViewCompras.Rows[i].Cells[0].Text == txtIdcompra.Text)
                     {
 
-                    sw.WriteLine(Environment.NewLine + "Identifiación del registro de compra: " + GridViewCompras.Rows[i].Cells[0].Text + "\t"
-                                 + Environment.NewLine + "Fecha del pedido: " + GridViewCompras.Rows[i].Cells[1].Text + "\t"
-                                 + Environment.NewLine + "Vehículo activo" + GridViewCompras.Rows[i].Cells[2].Text + "\t"
-                                 + Environment.NewLine + "Marca: " + GridViewCompras.Rows[i].Cells[3].Text + "\t"
-                                 + Environment.NewLine + "Modelo: " + GridViewCompras.Rows[i].Cells[4].Text + "\t"
-                                 + Environment.NewLine + Environment.NewLine + "Valor a pagar: " + GridViewCompras.Rows[i].Cells[5].Text + "\t"
-                                 + Environment.NewLine + Environment.NewLine + "Información del cliente" + Environment.NewLine + "Identificación: " + GridViewCompras.Rows[i].Cells[6].Text + "\t"
-                                 + Environment.NewLine + Environment.NewLine + "Nombre: " + GridViewCompras.Rows[i].Cells[7].Text + "\t"
-                                 + Environment.NewLine + "Apellidos: " + GridViewCompras.Rows[i].Cells[8].Text + "\t"
-                                 + Environment.NewLine + "Correo: " + GridViewCompras.Rows[i].Cells[9].Text + "\t"
-                                 + Environment.NewLine + "Dirección: " + GridViewCompras.Rows[i].Cells[10].Text + "\t"
-                                 + Environment.NewLine + "Número de cuenta: " + GridViewCompras.Rows[i].Cells[11].Text + "\t"
-                                 + Environment.NewLine + "Teléfono: " + GridViewCompras.Rows[i].Cells[12].Text + "\t"
-                                 + Environment.NewLine + Environment.NewLine +"      MUCHAS GRACIAS POR SU COMPRA :)"
-                                 + Environment.NewLine + Environment.NewLine);
-
+                        sw.WriteLine(Environment.NewLine + "Identifiación del registro de compra: " + GridViewCompras.Rows[i].Cells[0].Text + "\t"
+                                     + Environment.NewLine + "Fecha del pedido: " + GridViewCompras.Rows[i].Cells[1].Text + "\t"
+                                     + Environment.NewLine + "Vehículo activo" + GridViewCompras.Rows[i].Cells[2].Text + "\t"
+                                     + Environment.NewLine + "Marca: " + GridViewCompras.Rows[i].Cells[3].Text + "\t"
+                                     + Environment.NewLine + "Modelo: " + GridViewCompras.Rows[i].Cells[4].Text + "\t"
+                                     + Environment.NewLine + Environment.NewLine + "Valor a pagar: " + GridViewCompras.Rows[i].Cells[5].Text + "\t"
+                                     + Environment.NewLine + Environment.NewLine + "Información del cliente" + Environment.NewLine + "Identificación: " + GridViewCompras.Rows[i].Cells[6].Text + "\t"
+                                     + Environment.NewLine + Environment.NewLine + "Nombre: " + GridViewCompras.Rows[i].Cells[7].Text + "\t"
+                                     + Environment.NewLine + "Apellidos: " + GridViewCompras.Rows[i].Cells[8].Text + "\t"
+                                     + Environment.NewLine + "Correo: " + GridViewCompras.Rows[i].Cells[9].Text + "\t"
+                                     + Environment.NewLine + "Dirección: " + GridViewCompras.Rows[i].Cells[10].Text + "\t"
+                                     + Environment.NewLine + "Número de cuenta: " + GridViewCompras.Rows[i].Cells[11].Text + "\t"
+                                     + Environment.NewLine + "Teléfono: " + GridViewCompras.Rows[i].Cells[12].Text + "\t"
+                                     + Environment.NewLine + Environment.NewLine + "      MUCHAS GRACIAS POR SU COMPRA :)"
+                                     + Environment.NewLine + Environment.NewLine);
                     }
-
                 }
                 sw.Close();
-                //Mensaje de éxito
-                LblResultado.Text = "Factura exportada a un archivo plano.";
-                //
                 Page_Load(sender, e);
                 GridViewCompras.DataBind();
-                LblResultado.Text = "Venta de vehículo realizada con éxito..";                
+                LblResultado2.Text = "Venta de vehículo realizada con éxito..";
+                LblResultado.Text = "Factura exportada a un archivo plano.";
             }
-            else {
+            else
+            {
                 LblResultado.Text = "El Id de compra no está activo.";
-            }          
+            }
         }
-        else {
+        else
+        {
             LblResultado.Text = "Id de compra no válido.";
         }
     }
